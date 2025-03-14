85f324a43258f146bb4088e090036e4b0f7cb253
Saar Villman <saara.villman95@gmail.com>
2025-03-14 15:10:14 +0200

    added some components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,33 @@
 
 
-import './App.css'
+//import './App.css'
+//import Navbar from "./components/Navbar";
+//function App() {
+// 
+//
+//  return (
+//    <>
+//      <Navbar />
+//      <h1 class="text-3xl font-bold underline">
+//    Hello world!
+//  </h1>
+//    </>
+//  )
+//}
+//
+//export default App
 
-function App() {
- 
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/footter";
 
+const App = () => {
   return (
-    <>
-    
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
-  )
-}
-
+    <div className="w-full container mx-auto px-8">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,72 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+//import { StrictMode } from 'react'
+//import { createRoot } from 'react-dom/client'
+//import './index.css'
+//import App from './App.jsx'
+//
+//createRoot(document.getElementById('root')).render(
+//  <StrictMode>
+//    <App />
+//  </StrictMode>,
+//)
+//
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import Record from "./components/Record";
+import RecordList from "./components/RecordList";
+import SigIn from './components/SigIn'
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <RecordList />,
+      },
+    ],
+  },
+  {
+    path: "/edit/:id",
+    element: <App />,
+    children: [
+      {
+        path: "/edit/:id",
+        element: <Record />,
+      },
+    ],
+  },
+  {
+    path: "/create",
+    element: <App />,
+    children: [
+      {
+        path: "/create",
+        element: <Record />,
+      },
+    ],
+  },
+  {
+    path: "/sigIn",
+    element: <App />,
+    children: [
+      {
+        path: "/sigIn",
+        element: <SigIn/>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
