69a8005bc7b4e32f842108341054f2824cb52665
Saar Villman <saara.villman95@gmail.com>
2025-09-10 14:26:38 +0300

    delete  about navbar

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -39,9 +39,6 @@ export default function Navbar() {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <NavLink to="/exploregames">About</NavLink>
-              </li>
               {user && (
                 <>
                   <li>
