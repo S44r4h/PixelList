25ffa39f7d09e25b53118ef3b10ddf2113f41292
Saar Villman <saara.villman95@gmail.com>
2025-11-06 12:29:50 +0200

    added loading

--- client/src/components/auth/SignIn.jsx
+++ client/src/components/auth/SignIn.jsx
@@ -12,10 +12,13 @@ export default function Register() {
   });
   let response;
 
+  let [isLoading, setIsLoading] = useState(false);
+
   const navigate = useNavigate();
   const { setUser, loading } = useContext(UserContext);
 
   const handleSubmit = async (e) => {
+    setIsLoading(true);
     e.preventDefault();
     const loginperson = { ...Loginform };
 
@@ -35,6 +38,7 @@ export default function Register() {
       setUser(userData);
       navigate("/dashboard");
     } else {
+      setIsLoading(false);
       toast.error(
         userData.message || "Unknown error" /* gives server json message */
       );
@@ -42,40 +46,49 @@ export default function Register() {
   };
 
   return (
-    <form className="h-100 grid place-items-center" onSubmit={handleSubmit}>
-      <fieldset className="fieldset w-full max-w-md bg-base-200 border border-base-300 p-6 rounded-box">
-        <legend className="fieldset-legend">Login</legend>
+    <>
+      {isLoading ? (
+        <div className="h-100 grid place-items-center">
+          <p>Waking up the server... please wait</p>
+          <span className="loading loading-spinner text-primary"></span>
+        </div>
+      ) : (
+        <form className="h-100 grid place-items-center" onSubmit={handleSubmit}>
+          <fieldset className="fieldset w-full max-w-md bg-base-200 border border-base-300 p-6 rounded-box">
+            <legend className="fieldset-legend">Login</legend>
 
-        <label className="fieldset-label">Email</label>
-        <input
-          type="email"
-          className="input w-full"
-          placeholder="Email"
-          onChange={(e) =>
-            SetLoginform({ ...Loginform, email: e.target.value })
-          }
-        />
+            <label className="fieldset-label">Email</label>
+            <input
+              type="email"
+              className="input w-full"
+              placeholder="Email"
+              onChange={(e) =>
+                SetLoginform({ ...Loginform, email: e.target.value })
+              }
+            />
 
-        <label className="fieldset-label">Password</label>
-        <input
-          type="password"
-          className="input w-full"
-          placeholder="Password"
-          onChange={(e) =>
-            SetLoginform({ ...Loginform, password: e.target.value })
-          }
-        />
+            <label className="fieldset-label">Password</label>
+            <input
+              type="password"
+              className="input w-full"
+              placeholder="Password"
+              onChange={(e) =>
+                SetLoginform({ ...Loginform, password: e.target.value })
+              }
+            />
 
-        <button type="submit" className="btn btn-primary mt-4">
-          Login
-        </button>
-        <p className="text-center p-4">
-          Not a member?{" "}
-          <NavLink className="text-primary link link-hover" to="/register">
-            Register here!
-          </NavLink>
-        </p>
-      </fieldset>
-    </form>
+            <button type="submit" className="btn btn-primary mt-4">
+              Login
+            </button>
+            <p className="text-center p-4">
+              Not a member?{" "}
+              <NavLink className="text-primary link link-hover" to="/register">
+                Register here!
+              </NavLink>
+            </p>
+          </fieldset>
+        </form>
+      )}
+    </>
   );
 }
